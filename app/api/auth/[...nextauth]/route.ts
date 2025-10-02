819ca7cb47cb3c60e314fb34bc12a8a477df386f
stepandre1987-ctrl <step.andre1987@gmail.com>
2025-10-02 20:20:27 +0200

    Update route.ts

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,7 +1,6 @@
 export const runtime = "nodejs";
 
 import NextAuth from "next-auth";
-import { authOptions } from "@/lib/auth";
-
+import { authOptions } from "@/lib/server-auth"; // << sem
 const handler = NextAuth(authOptions);
 export { handler as GET, handler as POST };

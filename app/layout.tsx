c529ad3267af8438d0de4352e75519ca30e854a5
stepandre1987-ctrl <step.andre1987@gmail.com>
2025-10-02 16:55:52 +0200

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,7 +1,6 @@
 import "./globals.css";
 import type { Metadata } from "next";
 import { ReactNode } from "react";
-import Link from "next/link";
 
 export const metadata: Metadata = {
   title: "Drivers",
@@ -11,9 +10,8 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: { children: ReactNode }) {
   return (
     <html lang="cs">
-      <body>
-        {children}
-      </body>
+      <body>{children}</body>
     </html>
   );
 }
+
